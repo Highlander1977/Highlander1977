37ad47d767cb8a935ac340f669e545fdbe2c659b
Highlander1977 <97694641+Highlander1977@users.noreply.github.com>
2022-01-13 11:03:35 -0700

    Create myApp.js

---
+++ myApp.js
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Highlander1977
+- 👀 I’m interested in ... Learning, Sports and entertainment
+- 🌱 I’m currently learning ...security
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...Highlander1977
+
+<!---
+Highlander1977/Highlander1977 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
